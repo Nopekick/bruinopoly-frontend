53009c399fbc87f67f72b07b3fec0689b0d3fdfd
Thomas Kamm <tgkamm@yahoo.com>
2023-01-24 21:55:10 -0800

    reopen socket on close

--- src/reducers/lobby.js
+++ src/reducers/lobby.js
@@ -679,7 +679,8 @@ export const joinRoom = ({id, name, password, token}) => async (dispatch) => {
     })
 
     socket.addEventListener('close',(e) => {
-        console.log("Websocket server connection closed")
+        console.log("Socket connection has closed")
+        dispatch(checkSocket())
     })
 
 
